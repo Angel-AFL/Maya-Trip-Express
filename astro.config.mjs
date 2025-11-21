ed5f5a516e4a63155e5e833a29cfc9e064e8a36a
Jesús Mauricio Piste Ek <jesusek216@gmail.com>
2025-11-21 10:02:16 -0600

    fix(contador)se tomo los cambios de el repositorio dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-<<<<<<< HEAD
     plugins: [tailwindcss()],
   },
 
   integrations: [react()],
 });
-=======
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
-});
->>>>>>> a1c1e4d2d7725a89955c4b3c8d28f5fb8aaddb08
