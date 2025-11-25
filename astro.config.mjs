19dcfd0d53f3c05459bf2d458dc882d54edf687e
POOX4567 <pooxalexis@gmail.com>
2025-11-25 15:02:43 -0600

    Primer commit en Maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["react", "react-dom", "react-leaflet", "leaflet"],
+    },
   },
 
-  integrations: [react()]
+  // Integramos React correctamente
+  integrations: [react()],
 });
